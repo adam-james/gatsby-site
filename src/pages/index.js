0b7f66c64f4827c89714fe28c2524d747f92991a
adam-james <adam.j.sulewski@gmail.com>
2019-08-02 06:11:55 -0600

    fetch star wars characters

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,43 @@
 import React from "react"
+import axios from "axios"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const fetchPeople = () => axios.get("https://swapi.co/api/people")
+
+const StarWars = ({ people }) => (
+  <section>
+    <h2>Star Wars Characters</h2>
+    <ul>
+      {people.map(person => (
+        <li key={person.url}>{person.name}</li>
+      ))}
+    </ul>
+  </section>
+)
+
+class StarWarsContainer extends React.Component {
+  state = {
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const resp = await fetchPeople()
+    const { results } = resp.data
+    this.setState({ people: results, loading: false })
+  }
+
+  render() {
+    if (this.state.loading) return <p>Loading...</p>
+    return <StarWars people={this.state.people} />
+  }
+}
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <StarWarsContainer />
   </Layout>
 )
 
