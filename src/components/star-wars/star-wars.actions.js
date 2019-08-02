95f067d03a7a02cc1ef6f5bc41e0d1e7e989144a
adam-james <adam.j.sulewski@gmail.com>
2019-08-02 07:27:49 -0600

    report failure

--- src/components/star-wars/star-wars.actions.js
+++ src/components/star-wars/star-wars.actions.js
@@ -4,6 +4,23 @@ export const loadPeople = () => ({
   [RSAA]: {
     endpoint: "https://swapi.co/api/people",
     method: "GET",
-    types: ["@@people/REQUEST", "@@people/SUCCESS", "@@people/FAILURE"],
+    types: [
+      "@@people/REQUEST",
+      {
+        type: "@@people/SUCCESS",
+        payload: (action, state, resp) => {
+          // TODO deserialize here
+          return resp.json()
+        },
+      },
+      {
+        type: "@@people/FAILURE",
+        meta: (action, state, resp) => {
+          const { endpoint } = action["@@redux-api-middleware/RSAA"]
+          console.error(`Error report required for ${endpoint}`)
+          return resp
+        },
+      },
+    ],
   },
 })
